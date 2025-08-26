72abceb5fda80a54693865a1468b6d495c1b3697
Rahul <imrahul2308@gmail.com>
2025-08-26 17:24:32 +0530

    initial commit

---
+++ src/main/java/com/rahulyrn/RahulSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rahulyrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahulSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahulSpringApplication.class, args);
+	}
+
+}
